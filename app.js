c3acc81fabea079874034c5a0a7b95d1dfc5b37c
kuniyoshi1 <knuiyosi111@gmail.com>
2019-02-01 10:43:33 +0900

    add homework

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ console.log(name[0].value);
 
 
 //getElementsByClassName
-let fuga = document.getElementsByClassName('fuga  ');
+let fuga = document.getElementsByClassName('fuga');
 console.log(fuga);
 for(let i = 0; i < fuga.length; i++){
   //console画面に、fugaの要素、(1,2,3)を表示してください。
@@ -34,6 +34,32 @@ let tako = document.querySelector('#tako .ika');
 console.log(tako);
 console.log(tako.tagName);
 console.log(tako.textContent);
+
+//宿題
+console.log('宿題');
+
+function sendValue(){
+  // value値を取得する
+  let result = document.getElementByName('email').value;
+  let tagp = document.createElement('p');
+  let textp = document.createTextNode(result);
+   document.body.appendChild(tagp).appendChild(textp);
+}
+
+//1.メソッド用意する。
+//2.メソッドの中に絶対実行されるであろう処理を書く。
+//3.取得したい値を取得してconsole
+//4.pタグに格納して出力
+
+
+
+
+
+
+
+
+
+
 
 
 
